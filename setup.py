16fff86391d0c1edc8b3e822a147f1521a011195
malwarefrank <42877127+malwarefrank@users.noreply.github.com>
2021-12-22 13:06:44 +0000

    add magic requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["pefile>=2019.4.18"]
+requirements = ["pefile>=2019.4.18", "python-magic>=0.4.16"]
 
 setup_requirements = [
     "pytest-runner",
