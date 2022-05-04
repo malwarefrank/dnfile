85792bc338d5821395ff89cb279707264b67cd39
Mike Hunhoff <mike.hunhoff@gmail.com>
2022-05-04 16:11:25 -0600

    update typedef members tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -7,5 +7,7 @@ DATA = CD / "data"
 def get_data_path_by_name(name):
     if name == "hello-world.exe":
         return DATA / "hello-world" / "hello-world.exe"
+    elif name == "ModuleCode_x86.exe":
+        return DATA / "mixed-mode" / "ModuleCode" / "bin" / "ModuleCode_x86.exe"
 
     raise ValueError("unknown test file")
