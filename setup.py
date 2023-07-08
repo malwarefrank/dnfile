cf6a7b6107549acba6bc5c5000ca40821b7399dc
malwarefrank <42877127+malwarefrank@users.noreply.github.com>
2023-07-08 03:55:12 +0000

    Remove Python version less than 3.8
    Dev dependencies require Python 3.8+

--- setup.py
+++ setup.py
@@ -25,16 +25,13 @@ test_requirements = [
 
 setup(
     author="MalwareFrank",
-    python_requires=">=3.5",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
