ba8ae777ab56f8183595767c07b813781522f0ee
malwarefrank <42877127+malwarefrank@users.noreply.github.com>
2026-05-20 22:39:50 +0000

    update and add test files

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -13,5 +13,13 @@ def get_data_path_by_name(name):
         return DATA / "mixed-mode" / "EmptyClass" / "bin" / "EmptyClass_x86.exe"
     elif name == "EmptyClass_amd64.exe":
         return DATA / "mixed-mode" / "EmptyClass" / "bin" / "EmptyClass_amd64.exe"
+    elif name == "1d41308bf4148b4c138f9307abc696a6e4c05a5a89ddeb8926317685abb1c241":
+        return DATA / "malware" / "1d41308bf4148b4c138f9307abc696a6e4c05a5a89ddeb8926317685abb1c241"
+    elif name == "387f15043f0198fd3a637b0758c2b6dde9ead795c3ed70803426fc355731b173.dll_":
+        return DATA / "malware" / "387f15043f0198fd3a637b0758c2b6dde9ead795c3ed70803426fc355731b173.dll_"
+    elif name == "7f4ba9fc95b30baf8922a6933a4ff1c6a7fef41fae487bb31014c4963357770f.dll_":
+        return DATA / "malware" / "7f4ba9fc95b30baf8922a6933a4ff1c6a7fef41fae487bb31014c4963357770f.dll_"
+    elif name == "minimal-res.exe":
+        return DATA / "minimal-resource" / "bin" / "minimal-res.exe"
 
     raise ValueError("unknown test file")
